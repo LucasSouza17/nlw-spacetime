fe880feed8a8453b3f5e2b419077b1bfd9708fbe
Lucas Souza <lucasnando800@hotmail.com>
2023-05-15 22:06:33 -0300

    criação da estrutura dos projetos

---
+++ mobile/App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View>
+      <Text className="text-stone-50">
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
