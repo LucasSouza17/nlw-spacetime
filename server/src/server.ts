750cb12396ed8b3225d690b8c99e5b9133e2bc5d
Lucas Souza <lucasnando800@hotmail.com>
2023-05-17 22:11:02 -0300

    Add Autenticação Github

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,23 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
